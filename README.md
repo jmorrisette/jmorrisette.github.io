fe028258cd2ed38d67b036dddc6bcfd60e66170f
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-13 10:51:38 -0600

    first try with p5.js

--- README.md
+++
@@ -1,1 +0,0 @@
-"# jmorrisette.github.io" 
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	background(color(255,0,0));
+	//console.log(mouseX);
+	var x = mouseX;
+	var y = mouseY;
+}
