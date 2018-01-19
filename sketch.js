5c1d7c6e196328a1d12cbe0c5fca7f75b5cc7cae
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-18 21:07:19 -0600

    swarm online example

--- sketch.js
+++ sketch.js
@@ -1,107 +1,72 @@
-/* OpenProcessing Tweak of *@*http://www.openprocessing.org/sketch/6707*@* */
+/* OpenProcessing Tweak of *@*http://www.openprocessing.org/sketch/2363*@* */
 /* !do not delete the line above, required for linking your tweak if you upload again */
+// Gravity Swarm
+// Claudio Gonzales, June 2009
+// Albuquerque, New Mexico
 
-var points = [];
-var md = false;
+var numParticles;
+var colour;
+var Z = [];
+var mouseHeld;
 
-function setup(){
-  createCanvas(800,600);
+function setup() {
+  mouseHeld = false;
+  numParticles = 1000;
+  colour = random(1);
+
+  createCanvas(500,500);
   background(255);
   smooth();
-  //noStroke();
 
-}
-
-function draw(){
-  if(md){
-    for(var f=0; f<500; f+=1){
-      //for(int g=0; g<height; g+=height/10){
-      points.push(new Point(random(0,width),random(0,height)) );
-      //}
-    }
-  }
-  //fill(255,10);
-  //rect(-1,1,width+1,height+1);
-  //background(255);
-  noiseDetail(8,0);
-  //noiseSeed(1);
-  for(var i=points.length-1; i>0; i--){
-    points[i].update();
-    if(points[i].finished){
-      points.splice(i, 1);
-    }
+  for(var i = 0; i < numParticles; i++) {
+    Z.push(new particle( random(width), random(height), 0, 0, 1 ));
   }
 
-  //println(points.size());
+  //frameRate(60);
+  colorMode(RGB,255);
+
 }
 
-function mousePressed(){
-  md = true;
-}
-function mouseReleased(){
-  md = false;
-}
-function keyPressed(){
-  background(255);
-  noiseSeed(round(random(1000)));
-  for(var i=points.length-1; i>0; i--){
-    //p.x = random(width);
-    //p.y = random(height);
-    points.splice(i, 1);
+function draw() {
+
+  filter(INVERT);
+
+  var r;
+
+  stroke(0);
+  fill(255);
+  rect(0,0,width,height);
+
+  colorMode(HSB,1);
+  for(var i = 0; i < Z.length; i++) {
+    if( mouseHeld && mouseButton == LEFT ) {
+      Z[i].gravitate( new particle( mouseX, mouseY, 0, 0, 1 ) );
+    }
+    else if(mouseHeld && mouseButton == RIGHT ) {
+      Z[i].repel( new particle( mouseX, mouseY, 0, 0, 1 ) );
+    }
+    else {
+      Z[i].deteriorate();
+    }
+    Z[i].update();
+    r = i/Z.length;
+    stroke( colour, pow(r,0.1), 1-r, 0.15 );
+    Z[i].display();
   }
-  //saveFrame("perlin####.png");
-}
+  colorMode(RGB,255);
 
-function Point(x_, y_)
-{
-  this.x = x_;
-  this.y = y_;
-  this.xv = 0;
-  this.yv = 0;
-
-  this.maxSpeed = 3000000;
-  this.finished = false;
-
-  this.update = function(){
-    stroke(0,16);
-    var r = random(1);
-    this.xv = cos(noise(this.x*.01,this.y*.01)*TWO_PI);
-    this.yv = -sin(noise(this.x*.01,this.y*.01)*TWO_PI);
-
-    if(this.x>width){
-      //this.x = 1;
-      this.finished = true;
-    }
-    else if(this.x<0){
-      //this.x = width-1;
-      this.finished = true;
-    }
-    if(this.y>height){
-      //this.y = 1;
-      this.finished = true;
-    }
-    else if(this.y<0){
-      //this.y = height-1;
-      this.finished = true;
-    }
-
-    if(this.xv>this.maxSpeed){
-      this.xv = this.maxSpeed;
-    }
-    else if(this.xv<-this.maxSpeed){
-      this.xv = -this.maxSpeed;
-    }
-    if(this.yv>this.maxSpeed){
-      this.yv = this.maxSpeed;
-    }
-    else if(this.yv<-this.maxSpeed){
-      this.yv = -this.maxSpeed;
-    }
-
-    this.x += this.xv;
-    this.y += this.yv;
-
-    line(this.x+this.xv, this.y+this.yv,this.x,this.y );
+  colour+=random(0.01);
+  if( colour > 1 ) {
+    colour = colour%1;
   }
 
+  filter(INVERT);
+}
+
+function mousePressed(x, y) {
+  mouseHeld = true;
+}
+
+function mouseReleased(x, y) {
+  mouseHeld = false;
 }
