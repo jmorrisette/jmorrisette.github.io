e8cb72429b27140c560c9a89181e852cb7d01e41
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-18 19:05:56 -0600

    switching to jitterbugs

--- sketch.js
+++ sketch.js
@@ -1,10 +1,30 @@
+var bug;  // Declare object
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(window.width, window.height);
+  // Create object
+  bug = new Jitter();
 }
 
 function draw() {
-	background(color(255,0,0));
-	//console.log(mouseX);
-	var x = mouseX;
-	var y = mouseY;
+  background(50, 89, 100);
+  bug.move();
+  bug.display();
 }
+
+// Jitter class
+function Jitter() {
+  this.x = random(width);
+  this.y = random(height);
+  this.diameter = random(10, 30);
+  this.speed = 1;
+
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  };
+
+  this.display = function() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+};
