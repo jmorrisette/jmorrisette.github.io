5eadb6c1f8916d776ce8fdbd27cf3b8ade4d9fd2
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-20 01:49:09 -0600

    major changes

--- burst.js
+++ burst.js
@@ -1,4 +1,7 @@
 
+var test = [ {id:1, posX: 10, posY: 50, radius: 10},
+            {id:2, posX: 20 posY: 100, radius: 20},
+            {id:2, posX: 30 posY: 200, radius: 30} ];
 var particles = [];
 var numParticles = 10;
 
@@ -14,10 +17,24 @@ function setup() {
 function draw() {
   background(color(0,0,0));
   for (var i = 0; i < particles.length; i++) {
-    particles[i].move();
-    particles[i].display();
+    var p = particles[i];
+    p.move();
+    if (p.isDead) {
+      //source: http://stackoverflow.com/questions/16491758/remove-objects-from-array-by-object-property
+      //
+      // we have an array of objects called test,
+      // we want to remove one object using only the id property
+      // get index of object with id:2
+        //var removeIndex = test.map(function(item) { return item.id; }).indexOf(2);
+        // remove object
+        //test.splice(removeIndex, 1);
+        console.log("paticle #" + i + "died.");
+        
+    }
+    else {
+      particles[i].display();
+    }
   }
-
 }
 
 //CLASSES***********************
@@ -28,12 +45,21 @@ function particle(tempX, tempY, tempDiameter, tempXVelocity, tempYVelocity){
   this.diameter = tempDiameter;
   this.xVelocity = tempXVelocity;
   this.yVelocity = tempYVelocity;
-
+  this.isDead = false;
+  //hitpoints
+  this.hp = 255;
   this.move = function(){
     this.x += this.xVelocity;
     this.y += this.yVelocity;
-  };
+    lifespan --;
+
+    if (lifespan <= 0) {
+      this.isDead = true;
+    };
+
+
   this.display = function(){
+    fill(color(lifespan, lifespan, lifespan));
     ellipse(this.x,this.y,this.diameter,this.diameter);
   };
 }
