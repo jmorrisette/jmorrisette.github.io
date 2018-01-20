6c1df38db39025f66edebf12107472bc63b15ef7
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-20 00:49:40 -0600

    intial velocities now set to positive or negative

--- burst.js
+++ burst.js
@@ -1,11 +1,18 @@
 
 
+function getRandomVelocity() {
+  var a = Math.random()*2;
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+  var result = a*plusOrMinus;
+  return result;
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(color(0,0,0));
 
-  p = new particle(width/2, height/2, 20, Math.random()*2, Math.random()*2);
+  p = new particle(width/2, height/2, 20, getRandomVelocity, getRandomVelocity);
 }
 
 function draw() {
