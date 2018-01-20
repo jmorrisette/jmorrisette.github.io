1422de46d35a02a2a3757f5a4a556de7e5513771
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-20 02:48:25 -0600

    trying to embed two sketches in the same page

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+// Sketch One
+var m = function( p ) { // p could be any variable name
+
 var u;
 var l;
 var a;
@@ -7,7 +10,7 @@ var y;
 var count;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500,300);
   //u = int(width/15);
   u = 100;
   l = 20;
@@ -71,5 +74,106 @@ Module.prototype.draw2 = function() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  //resizeCanvas(windowWidth, windowHeight);
 }
+};
+var myp5 = new p5(m, 'magnets');
+
+
+//*************************************************************************
+//*************************************************************************
+//*************************************************************************
+// Sketch Two
+var b = function( p ) { // p could be any variable name
+
+var particles = [];
+var numParticles = 100;
+
+
+function setup() {
+  createCanvas(500,300);
+  for (var i = 0; i < numParticles; i++) {
+    temp = new particle(width/2, height/2, 10, getRandomVelocity(), getRandomVelocity());
+    particles.push(temp);
+  }
+}
+
+function draw() {
+  background(color(0,0,0));
+  for (var i = 0; i < particles.length; i++) {
+    var p = particles[i];
+    p.move();
+    if (!p.isDead) {
+      particles[i].display();
+    }
+  }
+  if (allDead(particles)) {
+    boom();
+  }
+}
+
+//check to see if each particle in the particles array isDead
+function allDead(particlesToCheck) {
+  for (var i = 0; i < particlesToCheck.length; i++) {
+    if (particlesToCheck[i].isDead == false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//initialize a new list of particles
+function boom() {
+  particles = new Array();
+  for (var i = 0; i < numParticles; i++) {
+    temp = new particle(width/2, height/2, 10, getRandomVelocity(), getRandomVelocity());
+    particles.push(temp);
+  }
+}
+
+
+
+
+//CLASSES***********************
+//particle class
+function particle(tempX, tempY, tempDiameter, tempXVelocity, tempYVelocity){
+  this.x = tempX;
+  this.y = tempY;
+  this.diameter = tempDiameter;
+  this.xVelocity = tempXVelocity;
+  this.yVelocity = tempYVelocity;
+  this.isDead = false;
+  //hitpoints
+  this.hp = 255;
+
+  this.move = function(){
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+    this.hp --;
+
+    if (this.hp <= 0) {
+      this.isDead = true;
+    };
+  }
+
+
+  this.display = function(){
+    fill(color(this.hp, this.hp, this.hp));
+    ellipse(this.x,this.y,this.diameter,this.diameter);
+  }
+}
+
+//helper function used when initializing a new particle
+function getRandomVelocity() {
+  var a = Math.random()*2;
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+  var result = a*plusOrMinus;
+  return result;
+}
+//END CLASSES*******************
+
+};
+var myp5 = new p5(b, 'burst');
+//*************************************************************************
+//*************************************************************************
+//*************************************************************************
