8111ecf25959396ff3c01b1bc949ce7ed7c75d9b
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-20 00:42:04 -0600

    fix

--- burst.js
+++ burst.js
@@ -5,7 +5,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(color(0,0,0));
 
-  p = new particle(width/2,height/2,20);
+  p = new particle(width/2, height/2, 20, Math.random()*2, Math.random()*2);
 }
 
 function draw() {
@@ -16,20 +16,16 @@ function draw() {
 
 
 //particle class
-function particle(tempX,tempY,tempDiameter){
-
+function particle(tempX, tempY, tempDiameter, tempXVelocity, tempYVelocity){
   this.x = tempX;
   this.y = tempY;
   this.diameter = tempDiameter;
-  var xVelocity = 2.5;
-  var yVelocity = 1;
-
-
+  this.xVelocity = tempXVelocity;
+  this.yVelocity = tempYVelocity;
   this.move = function(){
     this.x += xVelocity;
     this.y += yVelocity;
   };
-
   this.display = function(){
     ellipse(this.x,this.y,this.diameter,this.diameter);
   };
