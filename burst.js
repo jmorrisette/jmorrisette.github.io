b33e0180b7a4a08ffa7681492d0dfedaba292906
Jack Morrisette <jmorrisette13@gmail.com>
2018-01-20 00:59:13 -0600

    adding array of particles

--- burst.js
+++ burst.js
@@ -1,28 +1,26 @@
 
-
-function getRandomVelocity() {
-  var a = Math.random()*2;
-  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-  var result = a*plusOrMinus;
-  return result;
-}
+var particles = [];
+var numParticles = 10;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-
-  p = new particle(width/2, height/2, 20, getRandomVelocity(), getRandomVelocity());
+  for (var i = 0; i < numParticles; i++) {
+    temp = new particle(width/2, height/2, 10, getRandomVelocity(), getRandomVelocity());
+    particles.add(temp);
+  }
 }
 
 function draw() {
   background(color(0,0,0));
-  p.move();
-  p.display();
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].move();
+    particles[i].display();
+  }
 
 }
 
-
+//CLASSES***********************
 //particle class
 function particle(tempX, tempY, tempDiameter, tempXVelocity, tempYVelocity){
   this.x = tempX;
@@ -39,3 +37,12 @@ function particle(tempX, tempY, tempDiameter, tempXVelocity, tempYVelocity){
     ellipse(this.x,this.y,this.diameter,this.diameter);
   };
 }
+
+//helper function used when initializing a new particle
+function getRandomVelocity() {
+  var a = Math.random()*2;
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+  var result = a*plusOrMinus;
+  return result;
+}
+//END CLASSES*******************
